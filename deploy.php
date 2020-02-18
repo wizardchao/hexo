a9de19f663ff00c7f64ad0645f0017639126a9d2
wizardchao <18317119662@139.com>
2020-02-18 21:54:09 +0800

    add

--- deploy.php
+++ deploy.php
@@ -1,13 +1,18 @@
 <?php
-
+if (!$_POST) {
+	echo json_encode(array('code' => -1));exit;
+}
 $secret = "wanxiaoqian1124";
-$shellPath = "/www/vblog";
-set_time_limit(3 * 60); //最大过期时间3分钟
+$shellPath = "/www/vblog/deploy.sh";
+set_time_limit(30 * 60); //最大过期时间3分钟
 
 // 校验发送位置，正确的情况下自动拉取代码，实现自动部署
 $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+//获取GitHub发送的内容
+$json = file_get_contents('php://input');
+$content = json_decode($json, true);
 if ($signature) {
-	$hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
+	$hash = "sha1=" . hash_hmac('sha1', $content, $secret);
 	if (strcmp($signature, $hash) == 0) {
 		// sign sucess
 		set_time_limit(3 * 60); //最大过期时间3分钟
@@ -27,13 +32,13 @@ if ($signature) {
 		}
 
 	} else {
-		$parm = array(
+		$param = array(
 			'code' => 112,
 			'message' => '验签错误！',
 		);
 	}
 } else {
-	$parm = array(
+	$param = array(
 		'code' => 113,
 		'message' => '签名为空！',
 	);
